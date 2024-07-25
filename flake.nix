b2c5dab726eeb2011bdb18a339469481d79fab57
Ow1Dev <31339803+Ow1Dev@users.noreply.github.com>
2024-07-25 13:45:44 +0200

    fix: Add hyprland to flake

--- flake.nix
+++ flake.nix
@@ -17,6 +17,11 @@
 
     stylix.url = "github:danth/stylix";
 
+    # Hyprland ecosystem
+    hyprland = {
+      url = "git+https://github.com/hyprwm/Hyprland?submodules=1&ref=refs/tags/v0.41.1";
+    };
+
     # Just for pretty flake.nix
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
