3d4d8bbe955f58460f4821749c5af4eed52b3938
Ow1Dev <31339803+Ow1Dev@users.noreply.github.com>
2024-07-27 19:50:26 +0200

    fix: Remove sound and rtkit from sound

--- system/machine/nestop/modules/hardware/sound/default.nix
+++ system/machine/nestop/modules/hardware/sound/default.nix
@@ -1,8 +1,6 @@
 _: {
   # Sound settingsa
-  sound.enable = true;
   hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
